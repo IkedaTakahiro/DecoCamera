c17001ffe32a52827b8df3fd050e4095c33c54c1
池田享浩 <ikedatakahiro@ikedatooruhiroshi-no-macbook-air.local>
2015-12-26 14:53:26 +0900

    Initial Commit

---
+++ DecoCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DecoCamera
+//
+//  Created by 池田享浩 on 2015/12/26.
+//  Copyright © 2015年 takahiro ikeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DecoCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DecoCamera
+//
+//  Created by 池田享浩 on 2015/12/26.
+//  Copyright © 2015年 takahiro ikeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
